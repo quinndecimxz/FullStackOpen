6a6e24ebd08da0afba3078bd80bc1ff693dfdc94
Zain Ali <zainaliak57@gmail.com>
2025-07-25 21:17:35 +0500

    Add files via upload

---
+++ Part01/src/App.jsx
@@ -0,0 +1,47 @@
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
+
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+  };
+
+  const Content = (props) => {
+    return (
+      <div>
+        <p>{props.part1.name}  {props.part1.exercises}</p>
+        <p>{props.part2.name}  {props.part2.exercises}</p>
+        <p>{props.part3.name}  {props.part3.exercises}</p>
+      </div>
+    );
+  };
+
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Total number of exercises  {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
+    </div>
+  );
+};
+
+export default App;
