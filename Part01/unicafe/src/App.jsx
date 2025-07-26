db73aa7e05eb700c0334ad5d36c1de038eacd87c
Zain Ali <zainaliak57@gmail.com>
2025-07-26 17:02:52 +0500

    Add files via upload

---
+++ Part01/unicafe/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const Header = ({ heading }) => {
+  return (
+    <div>
+      <h2>{heading}</h2>
+    </div>
+  );
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ heading, good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positive = total === 0 ? 0 : (good / total) * 100;
+
+  return (
+    <div>
+      <h2>{heading}</h2>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={average.toFixed(2)} />
+            <StatisticLine text="positive" value={positive.toFixed(1) + "%"} />
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Header heading="give feedback" />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      <Statistics heading="statistics" good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
+
+export default App;
